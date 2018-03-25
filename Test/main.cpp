b5b0d816b9af445afbcdeeab643a57c8ba145d42
Macintoshuser2 <joshuarootsoniczg@gmail.com>
2018-03-25 17:30:58 -0500

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Josh Root on 3/25/18.
+//  Copyright © 2018 Josh the Gamer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
